687d4c0c4bf0f07f22e4b9c44622207708aff2c1
Josh Levinson <joshua.lev1995@gmail.com>
2022-07-11 12:30:51 +1000

    restarting from scratch

--- controllers/sessions.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const bcrypt = require('bcrypt')
-
-const sessionRouter = express.Router()
-
-req.flash('error', 'Incorrect username or password')
-res.redirect(req.baseUrl + '/login')
